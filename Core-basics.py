aa686404846fe8f4dc8f1a5c7a6dcd3210dffa2a
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-02-23 23:19:24 +0100

    Create Core-basics.py

---
+++ Core-basics.py
@@ -0,0 +1,8 @@
+input() -> reads input
+int(input()) -> converts the input to integer
+float(input()) -> converts the input to float
+type() -> gives the data type of a value
+.upper() -> puts in the uppercase
+.lower() -> puts in the lowercase
+.capitalize() -> formats the normal sentence, the first letter will be capital, others will be lowercase
+.count() -> counts the given parameter in the value
