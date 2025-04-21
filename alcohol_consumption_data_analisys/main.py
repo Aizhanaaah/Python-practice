d86804afa419bdc7ce349e942585029cd7731630
Aizhan Amanova <131990972+Aizhanaaah@users.noreply.github.com>
2025-04-21 22:38:56 +0200

    Update main.py

--- alcohol_consumption_data_analisys/main.py
+++ alcohol_consumption_data_analisys/main.py
@@ -4,5 +4,5 @@ import pandas as pd
 df=pd.read_csv('Alcohol_consumption.csv')
 print('the whole data:')
 print(df)
-continents = df['WHO region']
-#right now I want to write a descriptive statistics of the alcohol consumption in different regions
+continents = df['WHO region'].max(5)
+
